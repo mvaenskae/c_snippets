0162580052f4190ae98a6057dcf5a48b83ebd9e1
Mickey Vänskä <mvaenskae@gmail.com>
2015-12-25 19:39:30 +0100

    Completed exercise 9 (including extra credit).
    Signed-off-by: Mickey Vänskä <mvaenskae@gmail.com>

---
+++ learncthehardway/ex09.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int numbers[4] = {0};
+    
+    char name[4] = {'a'};
+
+    // Extra credit Q6
+    // Segfaults on individual access (line 53)
+    //char *name = "XXX";
+
+    // Extra credit Q4
+    char hack[4] = {'i'};
+
+    printf("hack: %c %c %c %c\n",
+            hack[0], hack[1],
+            hack[2], hack[3]);
+
+    hack[0] = 'Z';
+    hack[1] = 'e';
+    hack[2] = 'd';
+    hack[3] = '\0';
+
+    printf("hack: %d\n", hack);
+
+    printf("hack: %d %d %d %d\n",
+            hack[0], hack[1],
+            hack[2], hack[3]);
+
+    printf("hack: %c %c %c %c\n",
+            hack[0], hack[1],
+            hack[2], hack[3]);
+
+    // first, print them out raw
+    printf("numbers: %d %d %d %d\n",
+            numbers[0], numbers[1],
+            numbers[2], numbers[3]);
+
+    printf("name each: %c %c %c %c\n",
+            name[0], name[1],
+            name[2], name[3]);
+
+    printf("name: %s\n", name);
+
+   // setup the numbers
+    numbers[0] = 1;
+    numbers[1] = 2;
+    numbers[2] = 3;
+    numbers[3] = 4;
+
+    // setup the name
+    name[0] = 'Z';
+    name[1] = 'e';
+    name[2] = 'd';
+    name[3] = '\0';
+
+    // then print them out initialized
+    printf("numbers: %d %d %d %d\n",
+            numbers[0], numbers[1],
+            numbers[2], numbers[3]);
+
+    printf("name each: %c %c %c %c\n",
+            name[0], name[1],
+            name[2], name[3]);
+
+    // print the name like a string
+    printf("name: %s\n", name);
+     
+    // another way to use name
+    char *another = "Zed";
+
+    printf("another: %s\n", another);
+
+    printf("another each: %c %c %c %c\n",
+            another[0], another[1],
+            another[2], another[3]);
+
+    return 0;
+}
