42abba21bb010a31d1a4a12a66f99c5841bec3ee
Mickey Vänskä <mvaenskae@gmail.com>
2016-01-11 15:38:10 +0100

    Completed exercise 14 (including extra credit).
    Signed-off-by: Mickey Vänskä <mvaenskae@gmail.com>

---
+++ learncthehardway/ex14.c
@@ -0,0 +1,40 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+// forward declarations
+int can_print_it(char ch);
+void print_letters(int length, char arg[]);
+
+void print_arguments(int argc, char *argv[])
+{
+    int i = 0;
+    int length = 0;
+
+    for(i = 0; i < argc; i++) {
+        length = strlen(argv[i]);
+        print_letters(length, argv[i]);
+    }
+}
+
+void print_letters(int length, char arg[])
+{
+    int i = 0;
+
+    for(i = 0; i < length; i++) {
+        char ch = arg[i];
+
+        //if(isalpha(ch) || isblank(ch) || isdigit(ch)) {
+        if(isalnum(ch) || isblank(ch)) {
+            printf("'%c' == %d ", ch, ch);
+        }
+    }
+
+    printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+    print_arguments(argc, argv);
+    return 0;
+}
